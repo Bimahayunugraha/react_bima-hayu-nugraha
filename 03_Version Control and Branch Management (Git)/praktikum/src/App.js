b0eb64bff8861adba58b532a1068a93783e05e85
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-08-24 11:31:52 +0700

    memperbarui folder praktikum

---
+++ 03_Version Control and Branch Management (Git)/praktikum/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Welcome</h1>
+        <h2>Ini adalah project web</h2>
+        <h3>Project web menggunakan react</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
