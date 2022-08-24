0afa00bcbd85f038f08a39832e8c07b72d2cfb02
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-08-24 11:37:23 +0700

    memperbarui source code pada folder praktikum

--- 03_Version Control and Branch Management (Git)/praktikum/src/App.js
+++ 03_Version Control and Branch Management (Git)/praktikum/src/App.js
@@ -9,6 +9,25 @@ function App() {
         <h2>Ini adalah project web</h2>
         <h3>Project web menggunakan react</h3>
       </header>
+
+      {/* develop */}
+      <section className="container">
+        {/* feature one */}
+        <ul>
+          <h1>Feature One</h1>
+          <li>Try Out</li>
+          <li>Modul</li>
+          <li>Latihan Soal</li>
+        </ul>
+
+        {/* feature two */}
+        <ul>
+          <h1>Feature Two</h1>
+          <li>Pretest</li>
+          <li>Postest</li>
+          <li>Jadwal</li>
+        </ul>
+      </section>
     </div>
   );
 }
