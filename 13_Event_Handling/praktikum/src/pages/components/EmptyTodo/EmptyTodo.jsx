b43b547616523a91a213d4600f3136d19e77fa6f
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-09-19 13:34:48 +0700

    task/13_Event_Handling: menghapus task praktikum karena kesalahan upload tasknya

--- 13_Event_Handling/praktikum/src/pages/components/EmptyTodo/EmptyTodo.jsx
+++
@@ -1,5 +0,0 @@
-const EmptyTodo = () => {
-	return <div className="d-flex justify-content-center pt-4 fw-semibold">Oopss!!! Daftar tugas Anda kosong</div>;
-};
-
-export default EmptyTodo;
--- 13_Event_Handling/praktikum/src/pages/components/ListItem/ListItem.jsx
+++
@@ -1,5 +0,0 @@
-const LisItem = ({ item }) => {
-	return <div className="fw-semibold ps-2">{item.title}</div>;
-};
-
-export default LisItem;
