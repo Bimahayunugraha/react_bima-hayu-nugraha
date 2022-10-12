13cd86643b0ec5948f37530714b23b2987bf3748
Bima Hayu Nugraha <bimahayunugraha@gmail.com>
2022-10-12 22:06:09 +0700

    task/19_Deployment: menambahkan task praktikum

---
+++ 19_Deployment/praktikum/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Hello, Welcome to Static Website Bima Hayu Nugraha</p>
+				<a className="App-link" href="https://github.com/Bimahayunugraha" target="_blank" rel="noopener noreferrer">
+					My Github
+				</a>
+			</header>
+		</div>
+	);
+}
+
+export default App;
